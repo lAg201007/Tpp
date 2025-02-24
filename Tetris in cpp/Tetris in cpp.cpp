a3deb35f3aee623fd55c9242fe35d6dd9ffd409b
Rafael201007 <87946342+Rafael201007@users.noreply.github.com>
2025-02-24 09:43:22 -0300

    new class "Piece" new vectors with all shapes

--- Tetris in cpp/Tetris in cpp.cpp
+++ Tetris in cpp/Tetris in cpp.cpp
@@ -51,6 +51,66 @@ public:
     }
 };
 
+Texture empty_tile("Sprites/empty_tile.png");
+Texture tile1("Sprites/tile1.png");
+
+class Piece {
+public:
+    int posX, posY;
+    std::vector<std::pair<int, int>> pieceShape; 
+    std::vector<std::vector<Tile>>& tileMap;
+
+    Piece(std::vector<std::vector<Tile>>& map, int Xpos, int Ypos, std::vector<std::pair<int, int>> shape)
+        : tileMap(map), posX(Xpos), posY(Ypos), pieceShape(shape) {
+        placeOnTileMap();
+    }
+
+    void placeOnTileMap() {
+        for (const auto& [dx, dy] : pieceShape) {
+            int x = posX + dx;
+            int y = posY + dy;
+
+            if (y >= 0 && y < tileMap.size() && x >= 0 && x < tileMap[0].size()) {
+                tileMap[y][x].sprite->setTexture(*tile1.texture);
+            }
+        }
+    }
+
+    void move(int dx, int dy) {
+        posX += dx;
+        posY += dy;
+        placeOnTileMap(); 
+    }
+};
+
+std::vector<std::pair<int, int>> LShape = {
+    {0, 0}, {0, 1}, {0, 2}, {1, 2}
+};
+
+std::vector<std::pair<int, int>> JShape = {
+    {0, 0}, {0, 1}, {0, 2}, {-1, 2}
+};
+
+std::vector<std::pair<int, int>> OShape = {
+    {0, 0}, {0, 1}, {1, 0}, {1, 1}
+};
+
+std::vector<std::pair<int, int>> IShape = {
+    {0, 0}, {1, 0}, {2, 0}, {3, 0}
+};
+
+std::vector<std::pair<int, int>> TShape = {
+    {0, 0}, {1, 0}, {2, 0}, {1, 1}
+};
+
+std::vector<std::pair<int, int>> ZShape = {
+    {0, 1}, {1, 1}, {1, 0}, {2, 0}
+};
+
+std::vector<std::pair<int, int>> SShape = {
+    {0, 0}, {1, 0}, {1, 1}, {2, 1}
+};
+
 int main() {
     const int width = 256;
     const int height = 400; 
@@ -68,9 +128,7 @@ int main() {
         }
         tileMap.push_back(row);
     }
-
-    Texture empty_tile("Sprites/empty_tile.png");
-
+    
     // Loop pelos tiles antes de abrir a janela
     for (const auto& row : tileMap) {
         for (auto& tile : row) {
@@ -79,6 +137,10 @@ int main() {
             }
         }
     }
+
+    // exemplo de peça:
+    //          tile map, xpos, ypos, shape
+    // Piece LPiece(tileMap, 1, 1, LShape);
 
     std::unique_ptr window = std::make_unique<sf::RenderWindow>(sf::VideoMode({ width, height }), "Tetris");
 
