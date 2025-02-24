ad662fda107876ef1d4d0590fd42ab776e61457d
Rafael201007 <87946342+Rafael201007@users.noreply.github.com>
2025-02-24 00:58:47 -0300

    grid 2d funcionando:
    texturas adicionadas para o grid;
    
    classe de objeto adicionada;
    
    classe de objeto modificada para utilizar std::shared_ptr;
    
    classe filha tile adicionada;
    
    vetor tile map adicionado;
    
    sistema de criação de tiles com for loop adicionado;
    
    frame rate limit setado para 60;
    
    for loop para renderizar tiles adicionado;

--- Tetris in cpp/Tetris in cpp.cpp
+++ Tetris in cpp/Tetris in cpp.cpp
@@ -1,12 +1,66 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <memory>
+
+class Object {
+public:
+    std::shared_ptr<sf::Texture> spriteTexture;  // Shared pointer para textura
+    std::shared_ptr<sf::Sprite> sprite;
+
+    Object(std::string imgfile, float startXpos, float startYpos, int originX = 0, int originY = 0, float scaleX = 1, float scaleY = 1) {
+        spriteTexture = std::make_shared<sf::Texture>();  // Alocando a textura com shared_ptr
+
+        if (!spriteTexture->loadFromFile(imgfile)) {
+            std::cerr << "Não foi possível carregar a imagem: " << imgfile << std::endl;
+        }
+
+        sprite = std::make_shared<sf::Sprite>(*spriteTexture);
+
+        sprite->setPosition({ startXpos, startYpos });
+
+        
+        if (originX && originY) {
+            sprite->setOrigin(sf::Vector2f(static_cast<float>(originX), static_cast<float>(originY)));
+        }
+
+        sprite->setScale({ scaleX, scaleY });
+    }
+};
+
+class Tile : public Object {
+public:
+    int xGridPos;
+    int yGridPos;
+
+    Tile(int xGrid, int yGrid)
+        : Object("Sprites/tile1.png", xGrid * 8.0f, yGrid * 8.0f,4,4) { 
+        xGridPos = xGrid; yGridPos = yGrid;
+    }
+};
 
 int main()
 {
-    const int width = 400;
-    const int height = 600;
+    const int width = 256;
+    const int height = 240;
+    
+    int colums = width / 8;
+    int rows = height / 8;
+
+    std::vector<std::vector<Tile>> tileMap;
+
+    for (int i = 1; i < rows + 1; i++) {
+        std::vector<Tile> row;
+        for (int a = 1; a < colums + 1; a++) {
+            Tile tile(a,i);
+            row.emplace_back(tile);
+        }
+        tileMap.push_back(row);
+    }
+  
 
     std::unique_ptr window = std::make_unique<sf::RenderWindow> (sf::VideoMode({ width,height }), "Tetris");
+
+    window->setFramerateLimit(60);
 
     while (window->isOpen()) {
 
@@ -20,6 +74,11 @@ int main()
 
         window->clear();
 
+        for (const auto& row : tileMap) {
+            for (auto& tile : row) {
+                window->draw(*tile.sprite);
+            }
+        }
 
         window->display();
     }
